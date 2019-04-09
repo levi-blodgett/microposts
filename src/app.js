85c1f8cee9823075d03c5d05bc8cfeed1bf411a5
levi-blodgett <levi_blodgett@outlook.com>
2019-04-09 09:22:07 -0500

    Added get/post methods, and UI elemnts to verify additions and clear fields

--- src/app.js
+++ src/app.js
@@ -4,9 +4,34 @@ import { ui } from './ui';
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// Get Posts
 function getPosts() {
     // returns promise, so .then(), and .catch() for errors
     http.get('http://localhost:3000/posts')
         .then(data => ui.showPosts(data))
         .catch(err => console.log(err));
+}
+
+// Submit Post
+function submitPost() {
+    const title = document.querySelector('#title').value;
+    const body = document.querySelector('#body').value;
+
+    const data = {
+        // ES2015 syntax, if key and value are the same just type it like this
+        title,
+        body
+    }
+
+    // Create Post
+    http.post('http://localhost:3000/posts', data)
+    .then(data => {
+        ui.showAlert('Post added', 'alert alert-success');
+        ui.clearFields();
+        getPosts();
+    })
+    .catch(err => console.log(err));
 }
