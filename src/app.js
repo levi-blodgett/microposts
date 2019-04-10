154cd723fa06ebf512c93934b293b7273a65d6f8
levi-blodgett <levi_blodgett@outlook.com>
2019-04-10 08:25:02 -0500

    Added preliminary functionality to edit button, and had UI fill some now on button press

--- src/app.js
+++ src/app.js
@@ -7,6 +7,12 @@ document.addEventListener('DOMContentLoaded', getPosts);
 // Listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
 // Get Posts
 function getPosts() {
     // returns promise, so .then(), and .catch() for errors
@@ -36,9 +42,6 @@ function submitPost() {
     .catch(err => console.log(err));
 }
 
-// Listen for delete
-document.querySelector('#posts').addEventListener('click', deletePost);
-
 // Delete Post
 function deletePost(e) {
     e.preventDefault();
@@ -54,6 +57,25 @@ function deletePost(e) {
                 })
                 .catch(err => console.log(err));
         }
+    }
+}
+
+// Enable Edit State
+function enableEdit(e) {
+    e.preventDefault();
+    if (e.target.parentElement.classList.contains('edit')) {
+        const id = e.target.parentElement.dataset.id;
+        const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+        const body = e.target.parentElement.previousElementSibling.textContent;
+
+        const data = {
+            id,
+            title,
+            body
+        }
+
+        // Fill form with current post
+        ui.fillForm(data);
     }
 }
 
